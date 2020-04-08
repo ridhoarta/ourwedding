e81961f53e19d2e716a299ab8e6ab718a6117913
Ridho Octanio <dhotsky9@gmail.com>
2020-04-08 16:37:03 +0700

    Updates

---
+++ precache-manifest.a642ac53834b7176b04bbeee098ca1cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd6b5d74b0e38489c4a3610bd85b64ed",
+    "url": "/ourwedding/index.html"
+  },
+  {
+    "revision": "0b2ae97f474a8c459ceb",
+    "url": "/ourwedding/static/css/main.f78f7c5e.chunk.css"
+  },
+  {
+    "revision": "c9920265b6dbf4eee885",
+    "url": "/ourwedding/static/js/2.b548c93b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ourwedding/static/js/2.b548c93b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b2ae97f474a8c459ceb",
+    "url": "/ourwedding/static/js/main.40030b4a.chunk.js"
+  },
+  {
+    "revision": "94b6f1306f1ab31e67b1",
+    "url": "/ourwedding/static/js/runtime-main.44c9c021.js"
+  },
+  {
+    "revision": "76c145c2f3f1c17fd11ec9a3740521f6",
+    "url": "/ourwedding/static/media/DancingScript-Bold.76c145c2.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/ourwedding/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "44f03d8baacf045f63d4c0771b802b6c",
+    "url": "/ourwedding/static/media/profile.44f03d8b.jpg"
+  }
+]);
